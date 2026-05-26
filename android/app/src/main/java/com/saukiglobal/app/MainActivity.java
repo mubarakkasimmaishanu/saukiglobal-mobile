eac081157ee2b278fec411e93e33477408a75bab
mubarakkasim <khankingmubcy@gmail.com>
2026-05-26 22:06:16 +0100

    build: configure Capacitor and setup android native platform

---
+++ android/app/src/main/java/com/saukiglobal/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.saukiglobal.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saukiglobal.app',
+  appName: 'SaukiGlobal',
+  webDir: 'dist'
+};
+
+export default config;
