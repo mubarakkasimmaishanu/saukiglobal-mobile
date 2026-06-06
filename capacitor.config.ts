8b3db784913bc229c5623effa89d3fe2af56aa5f
mubarakkasim <khankingmubcy@gmail.com>
2026-06-06 17:04:32 +0100

    Once the build completes, the output APKs will be saved here: C:\Users\MY PC\Desktop\saukiglobal-mobile\android\app\build\outputs\apk\release\
    In that directory, you will see three main files:

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.saukiglobal.app',
   appName: 'SaukiGlobal',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'http://192.168.43.99:3000',
+    cleartext: true
+  }
 };
 
 export default config;
